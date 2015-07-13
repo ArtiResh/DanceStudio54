a2dd34043ccec1456b57a0dc39ea544d20ee0050
Artem Reshetov <art.reshetov39@gmail.com>
2015-07-13 19:57:31 +0200

    Add slider, threw out trash

--- js/jquery.showcase-slider.js
+++ js/jquery.showcase-slider.js
@@ -1,286 +1,275 @@
-///**
-// * Created by Максим on 09.07.2015.
-// */
-//
-//(function($) {
-//    $.fn.showcaseSlider = function(options){
-//
-//
-//
-//        function Showcase(element,options){
-//
-//            this.options = $.extend({},Showcade.Defaults,options);
-//
-//            this.$element = $(element);
-//
-//            this._current = null;
-//
-//            /**
-//             * Animation speed in milliseconds.
-//             * @protected
-//             */
-//            this._speed = null;
-//
-//            /**
-//             * Current width of the plugin element.
-//             */
-//            this._width = null;
-//
-//            /**
-//             * All real items.
-//             * @protected
-//             */
-//            this._items = [];
-//
-//            /**
-//             * All cloned items.
-//             * @protected
-//             */
-//            this._clones = [];
-//
-//            /**
-//             * Merge values of all items.
-//             * @todo Maybe this could be part of a plugin.
-//             * @protected
-//             */
-//            this._mergers = [];
-//
-//            /**
-//             * Invalidated parts within the update process.
-//             * @protected
-//             */
-//            this._invalidated = {};
-//
-//            /**
-//             * Ordered list of workers for the update process.
-//             * @protected
-//             */
-//            this._pipe = [];
-//
-//
-//            this.setup();
-//            this.initialize();
-//
-//        }
-//
-//        Showcase.Defualts = {
-//            marginLeft: 0,
-//            marginRight : 0,
-//            slider: ".Showcase-slider",
-//            blockImage:".Showcase-image"
-//        };
-//    };
-//    $.fn.showcaseSlider = function ( options ) {
-//        return this.each(function () {
-//            if (!$.data(this, 'plugin_' + Showcase)) {
-//                $.data(this, 'plugin_' + Showcase,
-//                    new Plugin( this, options ));
-//            }
-//        });
-//    }
-//})(jQuery);
-//
-//;(function ( $, window, document, element,options ) {
-//
-//    var pluginName = 'showcaseSlider';
-//
-//    function Showcase( element, options ) {
-//        this.element = element;
-//
-//        this.options = $.extend({},Showcade.Defaults,options);
-//
-//        this._defaults = defaults;
-//        this._name = pluginName;
-//
-//        this.init();
-//    }
-//
-//    Showcase.Defualts = {
-//        height: "50%",
-//        paddingTop: 0,
-//        marginLeft: 0,
-//        marginRight : 0,
-//        slider: ".Showcase-slider",
-//        blockImage:".Showcase-image"
-//    };
-//
-//    Showcase.prototype.init = function () {
-//        this.trigger('initialize');
-//
-//    };
-//
-//    Showcase.prototype.getProductId = function(elem){
-//        var countOfNum = 3;
-//        return parseInt($(elem).attr('id').substr(countOfNum));
-//    }
-//
-//    Showcase.prototype.getWidthInPercent = function () {
-//        var width = parseFloat($(this).css('width'))/parseFloat($(this).parent().css('width'));
-//        return (100*width);
-//    };
-//
-//    $.fn.showcaseSlider = function ( options ) {
-//        return this.each(function () {
-//            if (!$.data(this, 'plugin_' + pluginName)) {
-//                $.data(this, 'plugin_' + pluginName,
-//                    new Showcase( this, options ));
-//            }
-//        });
-//    }
-//
-//})( jQuery, window, document );
-
-
-
-/** */
 /**
- * Функция для получения порядковых номеров элементов (по умолчанию 3)
- * */
+ *  * Showcase Slider
+ * @version 1.0.0
+ * @author Reshetov Artem
+ *
+ */
 
-function getProductId(elem){
-    var countOfNum = 3;
-    return parseInt($(elem).attr('id').substr(countOfNum));
-}
+;
+(function ($, window, document, element, options) {
 
+    var pluginName = 'showcaseSlider';
 
-/**
- * Функция jQuery по переводу пикселей в проценты
- * */
-(function ($) {
-    $.fn.getWidthInPercent = function () {
-        var width = parseFloat($(this).css('width'))/parseFloat($(this).parent().css('width'));
-        return (100*width);
+    /**
+     * Базовый класс слайдера
+     * */
+
+    function Showcase(element, options) {
+        this.element = element;
+
+        this.options = $.extend({}, Showcase.Defaults, options);
+
+        this._name = pluginName;
+
+        this.naturalWidthFuture = [];
+
+        this.init();
+
+    }
+
+    /**
+     * Настройки по умолчанию, расширяются через options | unfinished
+     * */
+
+    Showcase.Defaults = {
+        heightLow: 50,
+        heightTall: 86,
+        paddingTop: 4.5,
+        marginLeft: 0.5,
+        marginRight: 1.5,
+        slider: ".directions__slider",
+        blockImage: ".backscreen",
+        navigationPanel:".directions__nav",
+        navigationClass: "active_dir",
+        countOfNum: 3,
+        nameOfCenterEl: "center",
+        sideOfMovement: "left"
     };
-})(jQuery);
 
+    /**
+     * Реализуемый прототип слайдера
+     * */
 
-/**
- * Слайдер
- * */
+    Showcase.prototype = {
 
-function Slider(sideOfMovement,selected) {
-    var marginLeft,marginRight, sideOfMovement;
-    marginLeft = 0.5;
-    marginRight = 1.5;
-    $(".center").animate({paddingTop:"4.5%", height:"50%"},150,function(){
-        if(sideOfMovement==="left"){
-            temp = -naturalWidthFuture[getProductId(".directions__slider .backscreen:last")]+"%";
-            $(".directions__slider").css({left: -naturalWidthFuture[getProductId(".directions__slider .backscreen:last")]*3+"%"}).prepend($(".directions__slider .backscreen:last"));
-            // $(".directions__slider").prepend($(".directions__slider .backscreen:last"));
-            /**Получение позиции смещения*/
-            offset = 0;
-            //var leftVar =(parseFloat($(".directions__slider").position().left)/parseFloat($(".directions__slider").parent().css('width'))*100);
-            //offset = 0;
-            //offset = (leftVar - offset)+'%';
-            selected.animate({width:naturalWidthFuture[getProductId(selected)]*2+"%",marginRight:marginRight+"%",marginLeft:marginLeft+"%"},500);
-            $(this).removeClass("center");
-            $(".directions__slider").animate({left:offset+"%"},500);
-            $(selected).animate({height:"86%",paddingTop:"0%"},400,function(){
-                $(this).addClass("center");
-                $(this).next().css({width:naturalWidthFuture[getProductId(this)]+"%",marginRight:0});
+        /**
+         * Функция — "Инициализация слайдера"
+         * */
+
+        init: function () {
+            var id;
+            var _this = this;
+            var settings = this.options;
+
+            /**Цикл по всем вложенным контейнерам слайдера,
+             * определяет их длину в процентах,
+             * и записывает в глобальную переменную: getWidthInPercent */
+            $(this.element).children().each(function () {
+                id = _this.getProductId(this, settings.countOfNum);
+                _this.naturalWidthFuture[id] = _this.getWidthInPercent(this);
+                if ($(this).hasClass(settings.nameOfCenterEl))_this.naturalWidthFuture[id] = _this.naturalWidthFuture[id] / 2;
+
+                /**определение обработчиков событий на блоки с картинками */
+                this.onclick = function () {
+                    _this.on(this, settings);
+                };
             });
 
-        }
-        else{
-            //$(".directions__slider").css({left: naturalWidthFuture[getProductId(".directions__slider .backscreen:last")]}).prepend($(".directions__slider .backscreen:last"));
-            //selected.width(naturalWidthFuture[getProductId(selected)]*2+"%");
+            /** определение обработчиков событий слайдер с меню */
+            $(settings.navigationPanel).children().each(function () {
+                this.onclick = function () {
+                    _this.navigate(this, settings);
+                };
+            });
+            return this.naturalWidthFuture = _this.naturalWidthFuture;
+        },
 
-            $(selected).css({width:naturalWidthFuture[getProductId(this)]*2+"%",marginRight:marginRight+"%",marginLeft:marginLeft+"%"});
+        /**
+         * Функция — "Определение направления движения"
+         * */
 
-            //$(selected).animate({marginLeft:marginLeft+"%"},10);
-            offset = -naturalWidthFuture[getProductId($(this))]*2;
-            //offset = 0;
-            $(".directions__slider").animate({left:offset+"%"},100,function(){
+        on: function (elem, settings) {
+            settings.sideOfMovement = $(elem).position().left - $("." + settings.nameOfCenterEl).position().left;
+            settings.sideOfMovement < 0 ? settings.sideOfMovement = "left" : settings.sideOfMovement = -"right";
+            this.slide(elem, settings);
+        },
 
-                $(".directions__slider").css({left:0}).append($(".directions__slider .backscreen:first"));
-                $(".center").animate({width:naturalWidthFuture[getProductId(".center")]+"%",marginRight:"0",marginLeft:"0"},300,function(){
+        /**
+         * Функция — "Получение идентификатора (id) у элемента"
+         * */
 
-                    $(this).removeClass("center");
-                    $(selected).animate({height:"86%",paddingTop:"0%",marginLeft:marginLeft+"%"},400,function(){
-                        $(this).addClass("center");
-                        $(this).next().css({width:naturalWidthFuture[getProductId(this)]+"%",marginRight:0});
-                    });
+        getProductId: function (elem, countOfNum) {
+            return parseInt($(elem).attr('id').substr(countOfNum));
+        },
+
+        /**
+         * Функция — "Получение длины в процентах"
+         * */
+
+        getWidthInPercent: function (elem) {
+            var width = parseFloat($(elem).css('width')) / parseFloat($(elem).parent().css('width'));
+            return (100 * width);
+        },
+
+        /**
+         *  Функция — "Обработчик события навигационной панели"
+         * */
+
+        navigate: function(elem, settings){
+            var _this = this;
+
+            /** не вызывает пункт мнюю который уже открыт*/
+            if( $(elem).hasClass("active_dir"))return true;
+
+            /** смена пунктов меню*/
+            $(settings.navigationPanel).children().removeClass(settings.navigationClass);
+            $(elem).addClass(settings.navigationClass);
+
+            /** определение переменных, получение номера максимального элемента */
+            var idCenter, idDir, idMax,leftSide,rightSide,itter,sideOfMovement;
+            idMax = $(settings.navigationPanel).children().length;
+
+            /** получение номера id центрального элемента и вызываемого пункта*/
+            idDir = parseInt(_this.getProductId($(elem), settings.countOfNum));
+            idCenter = parseInt(_this.getProductId($("."+settings.nameOfCenterEl), settings.countOfNum));
+
+            /** рассчет на сколько небходимо сдвинуться вправо и влево для смены*/
+            if(idDir<idCenter){
+                rightSide = idCenter - idDir;
+                leftSide = (idMax - idCenter) + idDir;
+            }
+            else{
+                leftSide = idDir - idCenter;
+                rightSide = (idMax - idDir) + idCenter;
+            }
+
+
+            /** выбор кратчайшего смещения и стороны смещения*/
+            itter = leftSide<rightSide?leftSide:rightSide;
+            settings.sideOfMovement = rightSide<leftSide?"left":"right";
+
+            /** прокрутка слайдера*/
+            for(var i=0;i<itter;i++){
+                setTimeout(_this.slide(("#bs_"+idDir),settings), 700);
+            }
+
+        },
+
+        /**
+         * Функция — "Реализация слайдера"
+         * */
+
+        slide: function (elem, settings) {
+            /** определение переменных, и сброс если был клинкнут центральный элемент*/
+            var selected = elem;
+            var _this = this;
+            var _mainEl = this.element;
+            if($(selected).hasClass(settings.nameOfCenterEl))return this;
+
+            /** уменьшение центральной картинки*/
+            $("." + settings.nameOfCenterEl).animate(
+                {
+                    paddingTop: settings.paddingTop + "%",
+                    height: settings.heightLow + "%"
+                }, 150, function () {
+
+                    /** реализиция движения при выборе левого элемента*/
+                    if (settings.sideOfMovement === "left") {
+
+                        /** добавление в начало слайдера блока из конца (на случай если был выбран первый элемент) и смещение слайдера на его длину*/
+                        $(_mainEl).css({left: -(_this.naturalWidthFuture[_this.getProductId(".directions__slider .backscreen:last", settings.countOfNum)]) * 3 + "%"})
+                            .prepend($(".directions__slider .backscreen:last"));
+
+                        /** увеличение длины и отступов выбранного элемента*/
+                        $(selected).animate({
+                            width: _this.naturalWidthFuture[_this.getProductId(selected, settings.countOfNum)] * 2 + "%",
+                            marginRight: settings.marginRight + "%",
+                            marginLeft: settings.marginLeft + "%"
+                        }, 500);
+
+                        /** убираем класс с экс-центрального элемента и двигаем слайдер*/
+                        $(this).removeClass(settings.nameOfCenterEl);
+                        $(_mainEl).animate({left: "0"}, 500);
+
+                        /** увеличивааем центральный элемент, добавляем класс и уменьшаем длину экс-центрального элемента*/
+                        $(selected).animate({height: settings.heightTall + "%", paddingTop: 0}, 400, function () {
+                            $(this).addClass(settings.nameOfCenterEl);
+                            $(this).next().css({
+                                width: _this.naturalWidthFuture[_this.getProductId(this, settings.countOfNum)] + "%",
+                                marginRight: 0
+                            });
+                        });
+
+                    }
+
+                    /** реализация движения при выборе вправого элемента*/
+                    else {
+                        /** увеличение длины и отступов выбранного элемента*/
+                        $(selected).css({
+                            width: _this.naturalWidthFuture[_this.getProductId(this, settings.countOfNum)] * 2 + "%",
+                            marginRight: settings.marginRight + "%",
+                            marginLeft: settings.marginLeft + "%"
+                        });
+
+                        /** смещение слайдера на 2ую длину выбранного блока*/
+                        offset = -(_this.naturalWidthFuture[_this.getProductId($(this), settings.countOfNum)] * 2);
+                        $(_mainEl).animate({left: offset + "%"}, 100, function () {
+
+                            /** добавление в конец слайдера блока из конца (на случай если был выбран последний элемент) и смещение слайдера на его длину*/
+                            $(_mainEl).css({left: 0}).append($(".directions__slider .backscreen:first"));
+
+                            /** уменьшение длины и отступов экс-центрального элемента*/
+                            $("." + settings.nameOfCenterEl).animate({
+                                width: _this.naturalWidthFuture[_this.getProductId("." + settings.nameOfCenterEl, settings.countOfNum)] + "%",
+                                marginRight: "0",
+                                marginLeft: "0"
+                            }, 300, function () {
+
+                                /** увеличение выбранного элемента*/
+                                $(this).removeClass(settings.nameOfCenterEl);
+                                $(selected).animate({
+                                    height: settings.heightTall + "%",
+                                    paddingTop: 0,
+                                    marginLeft: settings.marginLeft + "%"
+                                }, 400, function () {
+
+                                    /** Добавление класса */
+                                    $(this).addClass(settings.nameOfCenterEl);
+                                    $(this).next().css({
+                                        width: _this.naturalWidthFuture[_this.getProductId(this, settings.countOfNum)] + "%",
+                                        marginRight: 0
+                                    });
+                                });
+                            });
+
+                        });
+
+
+                    }
+
                 });
-
-            });
-
-
-
-
         }
 
-    });
+    };
 
-};
+    /**
+     * Реализация прототипа класса
+     * */
 
+    $.fn.showcaseSlider = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                    new Showcase(this, options));
+            }
+        });
+    }
 
+})(jQuery, window, document);
 
-
-var naturalWidthFuture=[];
-$(document).ready(function(){
-    $(".directions__nav div").click(function(){
-       /** не вызывает пункт мнюю который уже открыт*/
-       if( $(this).hasClass("active_dir"))return false;
-       /** смена пунктов меню*/
-        $(".directions__nav div").removeClass("active_dir");
-        $(this).addClass("active_dir");
-
-        var idCenter, idDir, idMax,leftSide,rightSide,itter,sideOfMovement;
-        idMax = $('.directions__nav').children().length;
-
-        /** Получение номера id центрального элемента и вызываемого пункта*/
-        idDir = parseInt(getProductId($(this)));
-        idCenter = parseInt(getProductId(".center"));
-
-        /** Рассчет на сколько небходимо сдвинуться вправо и влево для смены*/
-        if(idDir<idCenter){
-            rightSide = idCenter - idDir;
-            leftSide = (idMax - idCenter) + idDir;
-        }
-        else{
-            leftSide = idDir - idCenter;
-            rightSide = (idMax - idDir) + idCenter;
-        }
-       // leftSide<rightSide?itter=leftSide:itter=rightSide;
-        /** Выбор кратчайшего смещения и сторону смещения*/
-        itter = leftSide<rightSide?leftSide:rightSide;
-        sideOfMovement = rightSide<leftSide?"left":"right";
-
-        for(var i=0;i<itter;i++){
-
-            setTimeout(Slider(sideOfMovement,$("#bs_"+idDir)), 700);
-
-        }
-    });
-
-
-
-    $(".backscreen").on('click', function(event){
-        if( $(this).hasClass("center"))return false;
-        if(jQuery.isEmptyObject(naturalWidthFuture)){
-            $(".backscreen").each(function(){
-                id = getProductId($(this));
-                naturalWidthFuture[id] = $(this).getWidthInPercent();
-                if ($(this).hasClass("center"))naturalWidthFuture[id] = naturalWidthFuture[id]/2;
-            });
-        }
-
-        //var selected = $(this);
-        //var marginLeft,marginRight, sideOfMovement;
-        var sideOfMovement;
-        //marginLeft = 0.5;
-        //marginRight = 1.5;
-
-       // (getProductId($(this)) < getProductId($(".center")))?leftPart.push($(this)):rightPart.push($(this));
-            /** Определяем в какую сторону смещение относительно родительского элемента */
-
-                sideOfMovement =$(this).position().left - $(".center").position().left;
-                sideOfMovement<0?sideOfMovement="left":sideOfMovement=-"right";
-
-                Slider(sideOfMovement,$(this));
-
-    });
+$(document).ready(function () {
+    options = {paddingTop: "4.5"};
+    $(".directions__slider").showcaseSlider(this, options);
 });
-
 
 
