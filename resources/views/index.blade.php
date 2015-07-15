710e2651c43329d9ff9f5513013f489986ef1925
Artem Reshetov <art.reshetov39@gmail.com>
2015-07-15 14:04:03 +0200

    Remove issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,7 +44,6 @@
         <div class="directions__nav">
             <div class="active_dir" id="nv_1">jazz modern</div>
             <div id="nv_2">Go-Go</div>
-            <div id="nv_2">Go-Go</div>
             <div id="nv_3">jazz funk</div>
             <div id="nv_4">dance show</div>
             <div id="nv_5">strip dance</div>
