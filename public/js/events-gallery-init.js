0cc3c106d60b1d5e4e3a940ccf32e4e7b94a60a8
Artem Reshetov <art.reshetov39@gmail.com>
2015-07-24 19:26:39 +0200

    Add init fancy

---
+++ public/js/events-gallery-init.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".gallery_img").fancybox({
+        helpers	: {
+            thumbs	: {
+                width	: 50,
+                height	: 50
+            },
+            buttons	: {}
+        }
+    });
+});
