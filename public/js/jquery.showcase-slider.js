7be1a48e3bb03aab998381bfe66d82e256baabde
Artem Reshetov <art.reshetov39@gmail.com>
2015-07-24 18:01:04 +0200

    Changes in page directions

--- public/js/jquery.showcase-slider.js
+++ public/js/jquery.showcase-slider.js
@@ -186,6 +186,14 @@
             var _mainEl = this.element;
             if($(selected).hasClass(settings.nameOfCenterEl))return this;
 
+            /****************** блок иссчезания текста **********************/
+
+            $("#desc_"+ this.options.idOfCenterEl).animate({opacity:0},700,function(){
+                $(this).css({display:"none"});
+            });
+
+            /****************** **********************/
+
             /** уменьшение центральной картинки*/
             $("." + settings.nameOfCenterEl).animate(
                 {
@@ -212,7 +220,7 @@
                               width: _this.naturalWidthFuture[_this.getProductId($(selected), settings.countOfNum)] * 2 + "%",
                               marginRight: settings.marginRight + "%",
                               marginLeft: settings.marginLeft + "%"
-                        }, 500);
+                        }, 400,"linear");
 
 
                         /** убираем класс с экс-центрального элемента*/
@@ -258,7 +266,7 @@
                                     height: settings.heightTall + "%",
                                     paddingTop: 0,
                                     marginLeft: settings.marginLeft + "%"
-                                }, 400, function () {
+                                }, 400,"linear", function () {
 
                                     /** Добавление класса */
                                     $(this).addClass(settings.nameOfCenterEl);
@@ -278,6 +286,12 @@
                             });
                     }
                 });
+
+            /****************** блок проявления текста **********************/
+
+            $("#desc_"+ _this.getProductId($(selected), settings.countOfNum)).css({display:"block"}).animate({opacity:1},2000);
+
+            /****************** **********************/
         }
     };
 
